aa1a8bc7e0c3916099e0ed6d5acae6aea7acdd14
Michael Zwizwai <3036678z@student.gla.ac.uk>
2026-03-12 14:34:03 +0000

    Added Models and Admin

---
+++ mybase/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+    name = models.CharField(max_length=60, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Topic, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class Page(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_image', blank=True)
+
+    def __str__(self):
+        return self.user.username
